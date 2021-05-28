173e633051ee4d3fe797393743ea58a6b10b3356
Rajan4405 <rajanshatma652@gmail.com>
2021-05-28 22:35:43 +0545

    Practice 2 solution

--- src/App.js
+++ src/App.js
@@ -1,88 +1,75 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Component/Person';
-
+import Validation from './ValidationComponet/Validation';
+import Char from './Char/Char';
 class App extends Component {
   state = {
-    persons: [
-      { id: '12swa', name: 'Rajan', age: 28 },
-      { id: '13sds', name: 'Santosh', age: 29 },
-      { id: '14abs', name: 'Hari', age: 26 },
-    ],
-    otherState: 'some other value',
-    showPersons: false,
+    userInput: '',
+  };
+  inputChangeHandler = (event) => {
+    this.setState({ userInput: event.target.value });
   };
 
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id;
-    });
-
-    const person = { ...this.state.persons[personIndex] };
-
-    // const person = Object.assign({}, this.state.persons[personIndex])
-
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({ persons: persons });
-  };
-
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
+  deleteCharHandler = (index) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updateText = text.join('');
+    this.setState({ userInput: updateText });
   };
 
   render() {
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return (
+        <Char
+          character={ch}
+          key={index}
+          clicked={() => this.deleteCharHandler(index)}
+        />
       );
-    }
+    });
     return (
-      <>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>
-            Toggle Persons
-          </button>
-          {persons}
-        </div>
-      </>
+      <div>
+        <ol>
+          <li>
+            Create an input field(in App Component) with a change listener which
+            outputs the length of the entered text below(e.g. in a paragraph
+          </li>
+          <li>
+            Create new Component(ValidationComponent) which recive the text
+            length as prop
+          </li>
+          <li>
+            Inside the ValidationComponent either output "Text long short" or
+            "Text long enogh" depending on the next length(e.g. take 5 as a
+            minimum length)
+          </li>
+          <li>
+            Create another Component(= CharComponent) and style it as an inlinr
+            box(display:inline-block, padding:16px, text-align: center,
+            margin:16px, border:1px solid black)
+          </li>
+          <li>
+            Render a list of CharComponent where each CharComponent recieves a
+            different letter of the entered text(in the initial input field) as
+            prop
+          </li>
+          <li>
+            When you click a CharComponent, it should be removed entered text
+          </li>
+        </ol>
+        <p>Hint: keep in mind JavaScript are basically arrays! </p>
+        <hr />
+        <input
+          type="text"
+          onChange={this.inputChangeHandler}
+          value={this.state.userInput}
+        />
+        <p>{this.state.userInput}</p>
+
+        <Validation inputLength={this.state.userInput.length} />
+        {charList}
+      </div>
     );
-    // // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
